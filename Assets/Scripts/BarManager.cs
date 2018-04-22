10f93a1ae21e38a1aafa915f5dc93a8a0c452863
Bartek <bartekp44@gmail.com>
2018-04-22 14:45:13 +0200

    Bar bro!

---
+++ Assets/Scripts/BarManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManager : MonoBehaviour {
+
+    public Unit unit;
+
+    public List<GameObject> elements;
+
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for(int i =0; i < elements.Count; i++)
+        {
+            if (i < unit.Health)
+                elements[i].SetActive(true);
+            else
+                elements[i].SetActive(false);
+        }
+	} 
+}
