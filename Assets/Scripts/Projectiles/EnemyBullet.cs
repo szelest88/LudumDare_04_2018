aa3847bc3246a32318a3ccc1a7c8c63a61cbe336
Shellest <szelest88@gmail.com>
2018-04-22 15:04:59 +0200

    WOowww !

--- Assets/Scripts/Projectiles/EnemyBullet.cs
+++ Assets/Scripts/Projectiles/EnemyBullet.cs
@@ -7,7 +7,7 @@ public class EnemyBullet : Projectile
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("PlayerBody"))
         {
             dealDamage(projectileDamage, collision.gameObject);
             gameObject.SetActive(false);
