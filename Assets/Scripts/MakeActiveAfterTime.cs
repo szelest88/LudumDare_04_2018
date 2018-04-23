59f0c8e6fe8b2f90093882203709c1260190274c
Sanic <krzysiek56000@gmail.com>
2018-04-23 21:52:29 +0200

    Player collider gets disabled after time. Player's head and body colliders get enabled after time (1 sec.). This makes sure, that player will get registered only once by game manager. Also game scene balance and positions changes.

---
+++ Assets/Scripts/MakeActiveAfterTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeActiveAfterTime : MonoBehaviour {
+
+	public List<GameObject> objectsToModify;
+
+	public float timeToModify = 1;
+
+	public bool valueToApply = true;
+
+	public bool isActive = true;
+
+	void Start()
+	{
+		if (objectsToModify.Count <= 0 || timeToModify <= 0)
+			isActive = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isActive)
+		{
+			timeToModify -= Time.deltaTime;
+
+			if (timeToModify <= 0)
+			{
+				isActive = false;
+
+				for (int i = 0; i < objectsToModify.Count; i++)
+					if (objectsToModify [i] != null)
+						objectsToModify [i].SetActive (valueToApply);
+			}
+		}
+	}
+}
